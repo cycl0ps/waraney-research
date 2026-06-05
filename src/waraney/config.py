61bac7c8c004dde338b4052987abdcc4c248e124
cycl0ps <xnajoan@yahoo.com>
2026-06-05 11:08:35 +0700

    feat: update waraney model

--- src/waraney/config.py
+++ src/waraney/config.py
@@ -1,10 +1,23 @@
 import yaml
+from dataclasses import dataclass
+
+
+@dataclass
+class ModelConfig:
+    backbone: str
+    num_labels: int
+
+
+@dataclass
+class TrainingConfig:
+    batch_size: int
+    epochs: int
+    learning_rate: float
 
 
 def load_config(path: str):
 
     with open(path, "r") as f:
-
         config = yaml.safe_load(f)
 
     return config
--- src/waraney/model.py
+++ src/waraney/model.py
@@ -1,10 +1,23 @@
+from transformers import AutoTokenizer
+
+
 class WaraneyModel:
 
-    def __init__(self):
-        self.name = "WARANEY"
+    def __init__(
+        self,
+        model_name: str
+    ):
+
+        self.model_name = model_name
+
+        self.tokenizer = (
+            AutoTokenizer.from_pretrained(
+                model_name
+            )
+        )
 
     def info(self):
 
         return {
-            "name": self.name
+            "model_name": self.model_name
         }
